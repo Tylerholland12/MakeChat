4d34833ff9dc81500a15df25409151d1c3f97051
Tyler_Holland <tyler.holland@students.makeschool.com>
2021-04-27 09:38:32 -0500

    [update] Integrated Socket.io

--- app.js
+++ app.js
@@ -5,13 +5,22 @@ const app = express();
 //Socket.io has to use the http server
 const server = require('http').Server(app);
 
+//Socket.io
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected! 🔌");
+})
+
 //Express View Engine for Handlebars
 const exphbs  = require('express-handlebars');
-app.engine('handlebars', exphbs());
+app.engine('hbs', exphbs({defaultLayout: 'index', extname: '.hbs'}));
 app.set('view engine', 'handlebars');
 
+//Establish your public folder
+app.use('/public', express.static('public'))
+
 app.get('/', (req, res) => {
-  res.render('index.handlebars');
+  res.render('index.hbs');
 })
 
 server.listen('3000', () => {
