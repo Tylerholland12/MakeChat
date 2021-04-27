6bd3b544709b56952a9b28cd10129ae02187fa03
Tyler_Holland <tyler.holland@students.makeschool.com>
2021-04-27 10:08:23 -0500

    [update] Users can join chat and send messages

--- public/index.js
+++ public/index.js
@@ -1,21 +1,49 @@
-// index.js
-$(document).ready(() => {
-
+//index.js
+$(document).ready(()=>{
   const socket = io.connect();
 
-  $('#create-user-btn').click((e) => {
+  //Keep track of the current user
+  let currentUser;
+
+  $('#create-user-btn').click((e)=>{
     e.preventDefault();
-    let username = $('#username-input').val();
-    if(username.length > 0){
-      //Emit to the server the new user
-      socket.emit('new user', username);
+    if($('#username-input').val().length > 0){
+      socket.emit('new user', $('#username-input').val());
+      // Save the current user when created
+      currentUser = $('#username-input').val();
       $('.username-form').remove();
+      $('.main-container').css('display', 'flex');
+    }
+  });
+
+  $('#send-chat-btn').click((e) => {
+    e.preventDefault();
+    // Get the message text value
+    let message = $('#chat-input').val();
+    // Make sure it's not empty
+    if(message.length > 0){
+      // Emit the message with the current user to the server
+      socket.emit('new message', {
+        sender : currentUser,
+        message : message,
+      });
+      $('#chat-input').val("");
     }
   });
 
   //socket listeners
   socket.on('new user', (username) => {
-    console.log(`✋ ${username} has joined the chat! ✋`);
+    console.log(`${username} has joined the chat`);
+    $('.users-online').append(`<div class="user-online">${username}</div>`);
+  })
+
+  socket.on('new message', (data) => {
+    $('.message-container').append(`
+    <div class="message">
+      <p class="message-user">${data.sender}: </p>
+      <p class="message-text">${data.message}</p>
+    </div>
+  `);
   })
 
 })
--- sockets/chat.js
+++ sockets/chat.js
@@ -9,4 +9,11 @@ module.exports = (io, socket) => {
     io.emit("new user", username);
   })
 
+  //Listen for new messages
+  socket.on('new message', (data) => {
+    // Send that data back to ALL clients
+    console.log(`${data.sender}: ${data.message}`)
+    io.emit("new message", data)
+  })
+
 }
